b8463e5afda08023af8bad9bf215cb2dcba456df
eyasq <qeremeyas99@gmail.com>
2025-03-18 00:43:46 +0200

    added models

--- exam_app/models.py
+++ exam_app/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+import re
 
 # Create your models here.
+
+class UserManager(models.Manager):
+    def registery_validator(self, postData):
+        errors = {}
+        first_name = postData.get('first_name')
+        if not first_name:
+            errors['first_name'] = 'First Name is a required Field!'
+        elif len(first_name) < 2:
+            errors['first_name'] = 'First Name must be at least 2 characters.'
+        elif len(first_name) > 32:
+            errors['first_name'] = 'First name can be 32 characters at max'
+        last_name = postData.get('last_name')
+        if not last_name:
+            errors['last_name'] = 'Last Name is a required Field!'
+        elif len(last_name) < 2:
+            errors['last_name'] = 'Last Name must be at least 2 characters.'
+        elif len(last_name) > 32:
+            errors['last_name'] = 'Last name can be 32 characters at max'
+        email = postData.get('email')
+        if not email:
+            errors['email'] = 'Email is a required Field!'
+        elif email and self.filter(email = email).exists():
+            errors['email'] = 'Email already exists!'
+        elif len(email) < 5:
+            errors['email'] = 'Email must be at least 5 characters.'
+        password = postData.get('password')
+        if not password:
+            errors['password'] = 'Password is a required Field!'
+        elif len(password) < 8:
+            errors['password'] = 'Password must be 8 chars atleast'
+        confirm_pw = postData.get('confirm_pw')
+        if confirm_pw != password:
+            errors['confirm_pw']= "Passwords do not match!"
+        return errors
+    
+    def login_validator(self, postData):
+        errors = {}
+        email = postData.get('email')
+        if not email:
+            errors['email'] = 'Email cannot be empty'
+        password = postData.get('password')
+        if not password:
+            errors['password'] = 'Password cannot be empty'
+    
+
+class PieManager(models.Manager):
+    def pie_validator(self, postData):
+        errors = {}
+        pie_name = postData.get('pie_name')
+        if not pie_name:
+            errors['pie_name'] = 'Pie Name is a required Field!'
+        elif len(pie_name) < 2:
+            errors['pie_name'] = 'Pie Name must be at least 2 characters.'
+        elif len(pie_name) > 32:
+            errors['pie_name'] = 'Pie Name can be 32 characters at max'
+        elif pie_name and self.objects.filter(name = pie_name).exists():
+            errors['pie_name'] = 'Pie Name must be unique! There is already a pie with this name.'
+        return errors
+   
+    
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+    
+class Pie(models.Model):
+    name = models.CharField(max_length=32)
+    filling = models.CharField(max_length=32)
+    crust = models.CharField(max_length=32)
+    votes = models.IntegerField(default = 0)
+    user = models.ForeignKey(User, related_name='pies', on_delete=models.CASCADE)
+    objects = PieManager()
